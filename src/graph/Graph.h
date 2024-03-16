572561b9913286610eab2ff0578587e79580fc72
BenP <paulson2@ualberta.ca>
2024-03-16 14:15:40 -0600

    fixed empty files

--- src/graph/Graph.h
+++ src/graph/Graph.h
@@ -2,11 +2,21 @@
 #define GRAPH_H
 
 #include <QObject>
+#include "src/graph/qcustomplot.h"
 
-class graph
+class Graph : public QObject
 {
+    Q_OBJECT
 public:
-    graph();
+    explicit Graph(QCustomPlot *customPlot, QObject *parent = nullptr);
+    ~Graph();
+
+    void setGraph(const int rangeX, const int rangeY);
+    void addGraph(const QVector<double> &x, const QVector<double> &y);
+private:
+    QCustomPlot *m_customPlot;
+    QVector<double> qt_time;
+    QVector<double> qt_data;
 };
 
 #endif // GRAPH_H
